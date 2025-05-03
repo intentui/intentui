143a85e2c6d980cacd797d24bb342c6e260651d3
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-05-03 19:10:52 +0700

    format

--- app/sink/page.tsx
+++ app/sink/page.tsx
@@ -1,9 +1,3 @@
-import TableBulkDemo from "@/components/docs/collections/table/table-bulk-demo"
-import TableDragDemo from "@/components/docs/collections/table/table-drag-demo"
-import TableResizeDemo from "@/components/docs/collections/table/table-resize-demo"
-
 export default function Page() {
-  return (
-    <div/>
-  )
+  return <div />
 }
