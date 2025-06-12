6f7750213ed028cbbb6486c4c84b9e5af9261999
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-06-12 20:52:03 +0700

    feat(core): clean

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,17 +1,5 @@
 "use client"
 
-import TagFieldDemo from "@/components/docs/forms/tag-field/tag-field-demo"
-import MultipleSelectDemo from "@/components/docs/pickers/multiple-select/multiple-select-demo"
-import { TextField } from "@/components/ui/text-field"
-
 export default function Page() {
-  return (
-    <div className="p-32">
-      <div className="flex items-center gap-x-1">
-        <MultipleSelectDemo />
-        <TagFieldDemo />
-        <TextField />
-      </div>
-    </div>
-  )
+  return <div />
 }
