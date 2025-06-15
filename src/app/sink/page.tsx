09b9633d4f39779471345b566148cb61b8ae69bd
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-06-16 01:39:29 +0700

    docs: rewrite docs

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,10 +1,5 @@
 "use client"
-import FileTriggerPendingDemo from "@/components/docs/buttons/file-trigger/file-trigger-pending-demo"
 
 export default function Page() {
-  return (
-    <div className="p-32">
-      <FileTriggerPendingDemo />
-    </div>
-  )
+  return <div />
 }
