0405b373fda05a1187cab627aa5a3cb96d175376
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-06-26 22:05:43 +0700

    docs(popover): fix padding

--- src/components/docs/overlays/popover/popover-custom-close.tsx
+++ src/components/docs/overlays/popover/popover-custom-close.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
+import { Dialog } from "@/components/ui/dialog"
 import { Form } from "@/components/ui/form"
 import { Link } from "@/components/ui/link"
 import { Popover } from "@/components/ui/popover"
@@ -11,40 +12,42 @@ export default function PopoverCustomClose() {
   return (
     <Popover>
       <Button>Login</Button>
-      <Popover.Content className="sm:min-w-96">
-        <Popover.Header>
-          <Popover.Title>Login</Popover.Title>
-          <Popover.Description>Enter your credentials to sign in.</Popover.Description>
-        </Popover.Header>
-        <Form onSubmit={() => {}}>
-          <Popover.Body>
-            <div className="space-y-4">
-              <TextField
-                autoFocus
-                isRequired
-                type="email"
-                label="Email"
-                placeholder="Enter your email"
-              />
-              <TextField
-                isRequired
-                label="Password"
-                type="password"
-                placeholder="Enter your password"
-              />
-              <div className="flex items-center justify-between">
-                <Checkbox name="remember-me">Remember me</Checkbox>
-                <Link intent="primary" href="#">
-                  Forgot password?
-                </Link>
+      <Popover.Content className="w-full min-w-96">
+        <Dialog>
+          <Popover.Header>
+            <Popover.Title>Login</Popover.Title>
+            <Popover.Description>Enter your credentials to sign in.</Popover.Description>
+          </Popover.Header>
+          <Form onSubmit={() => {}}>
+            <Popover.Body>
+              <div className="space-y-4">
+                <TextField
+                  autoFocus
+                  isRequired
+                  type="email"
+                  label="Email"
+                  placeholder="Enter your email"
+                />
+                <TextField
+                  isRequired
+                  label="Password"
+                  type="password"
+                  placeholder="Enter your password"
+                />
+                <div className="flex items-center justify-between">
+                  <Checkbox name="remember-me">Remember me</Checkbox>
+                  <Link intent="primary" href="#">
+                    Forgot password?
+                  </Link>
+                </div>
               </div>
-            </div>
-          </Popover.Body>
-          <Popover.Footer>
-            <Popover.Close>Cancel</Popover.Close>
-            <Button type="submit">Login</Button>
-          </Popover.Footer>
-        </Form>
+            </Popover.Body>
+            <Popover.Footer>
+              <Popover.Close>Cancel</Popover.Close>
+              <Button type="submit">Login</Button>
+            </Popover.Footer>
+          </Form>
+        </Dialog>
       </Popover.Content>
     </Popover>
   )
