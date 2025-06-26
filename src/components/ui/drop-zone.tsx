0ffb146f2b5bb78e7e32cc787e79eb5e00accdca
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-06-27 00:54:09 +0700

    fix(warning): dropzone

--- src/components/ui/drop-zone.tsx
+++ src/components/ui/drop-zone.tsx
@@ -7,7 +7,7 @@ const DropZone = ({ className, ...props }: DropZoneProps) => (
   <DropPrimitiveZone
     className={composeRenderProps(className, (className, { isDropTarget }) =>
       twMerge(
-        "group flex max-h-[200px] max-w-xl flex-col items-center justify-center gap-2 rounded-md border border-dashed p-6 text-sm has-[slot=description]:text-center",
+        "group flex max-h-[200px] max-w-xl flex-col items-center justify-center gap-2 rounded-md border border-dashed p-6 text-sm has-[[slot=description]]:text-center",
         isDropTarget &&
           "border-primary border-solid bg-primary/10 ring-3 ring-ring/20 [&_.text-muted-fg]:text-primary-fg",
         className,
