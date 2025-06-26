e211be2a54633896ea66f472d0e0bd6850506ac4
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-06-27 02:24:19 +0700

    refactor(clean): sink

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,15 +1,5 @@
 "use client"
 
-import { Checkbox } from "@/components/ui/checkbox"
-import { Description, Label } from "@/components/ui/field"
-
 export default function Sink() {
-  return (
-    <div>
-      <Checkbox value="read">
-        <Label>Read</Label>
-        <Description>Can view content but cannot make changes.</Description>
-      </Checkbox>
-    </div>
-  )
+  return <div />
 }
