e195cd4810cada68ad674da49c94e94d1edae657
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-06-27 17:08:42 +0700

    fix(modal): share props

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,3 +1,7 @@
+import ModalControlledDemo from "@/components/docs/overlays/modal/modal-controlled-demo";
+
 export default function Page() {
-  return <div />
+  return <div className='p-32'>
+    <ModalControlledDemo/>
+  </div>
 }
