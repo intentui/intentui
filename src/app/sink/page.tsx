1e3e89672b9ad21222d13355eaef89265e2f539b
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-07-05 12:09:13 +0700

    refactor(pagination): items

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,3 +1,13 @@
+import PaginationDemo from "@/components/docs/navigation/pagination/pagination-demo"
+import PaginationDynamicDemo from "@/components/docs/navigation/pagination/pagination-dynamic-demo"
+import SimplePaginationDemo from "@/components/docs/navigation/pagination/simple-pagination-demo"
+
 export default function Page() {
-  return <div />
+  return (
+    <div className="flex flex-col gap-y-2 p-32">
+      <PaginationDemo />
+      <SimplePaginationDemo />
+      <PaginationDynamicDemo />
+    </div>
+  )
 }
--- src/components/docs/navigation/link/link-button-demo.tsx
+++ src/components/docs/navigation/link/link-button-demo.tsx
@@ -6,21 +6,13 @@ import { Link } from "@/components/ui/link"
 export default function LinkButtonDemo() {
   return (
     <div className="flex gap-2">
-      <Link className={(renderProps) => buttonStyles({ ...renderProps })} href="#use-as-button">
+      <Link className={buttonStyles} href="#use-as-button">
         Link
       </Link>
-      <Link
-        className={(renderProps) => buttonStyles({ ...renderProps, intent: "outline" })}
-        href="#use-as-button"
-      >
+      <Link className={buttonStyles({ intent: "outline" })} href="#use-as-button">
         Link
       </Link>
-      <Link
-        className={(renderProps) =>
-          buttonStyles({ ...renderProps, intent: "plain", isCircle: true })
-        }
-        href="#use-as-button"
-      >
+      <Link className={buttonStyles({ intent: "plain", isCircle: true })} href="#use-as-button">
         Link
       </Link>
     </div>
