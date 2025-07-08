bf4aef92b448fcd14b1fac441ac197c83daa080c
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-07-08 09:57:25 +0700

    fix(number-field): mobile

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,3 +1,9 @@
+import NumberFieldDemo from "@/components/docs/forms/number-field/number-field-demo"
+
 export default function Page() {
-  return <div />
+  return (
+    <div className="p-6">
+      <NumberFieldDemo />
+    </div>
+  )
 }
