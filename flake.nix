f376103b9b1df120f3dc50116336d598b61eee2a
noah <kedom@vmcall.net>
2025-07-09 18:43:33 +0200

    chore: add nix flake and direnv support

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Development environment for intentui";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs
+            pkgs.bun
+          ];
+        };
+        formatter = pkgs.alejandra;
+      }
+    );
+}
