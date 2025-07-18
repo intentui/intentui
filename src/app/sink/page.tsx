032b8004dc2b7387fc011684d11ec49f54970f5d
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-07-18 19:08:37 +0700

    fix(tooltip): arrow

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,5 +1,11 @@
 "use client"
 
+import TooltipDemo from "@/components/docs/overlays/tooltip/tooltip-demo"
+
 export default function Page() {
-  return <div />
+  return (
+    <div className="p-32">
+      <TooltipDemo />
+    </div>
+  )
 }
