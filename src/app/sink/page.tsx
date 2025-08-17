4a97bb2d86832286232c3c5fa7648fb3b362c0dc
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-08-17 12:46:57 +0700

    fix(clean): sink

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,11 +1,5 @@
 "use client"
 
-import { TextField } from "@/components/ui/text-field"
-
 export default function Page() {
-  return (
-    <div className="p-32">
-      <TextField isRevealable type="password" className="max-w-xs" />
-    </div>
-  )
+  return <div />
 }
