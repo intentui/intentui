c50162fa6327d91098219296e2e01a6398ed89e3
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-08-17 12:44:51 +0700

    fix(text field): on reveable

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,5 +1,11 @@
 "use client"
 
+import { TextField } from "@/components/ui/text-field"
+
 export default function Page() {
-  return <div />
+  return (
+    <div className="p-32">
+      <TextField isRevealable type="password" className="max-w-xs" />
+    </div>
+  )
 }
