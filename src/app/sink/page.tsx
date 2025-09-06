7d866b2ed25a9c2cdf8567c7f245a2b6cd049727
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-09-07 04:36:54 +0700

    clean sink

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,13 +1,5 @@
 "use client"
 
-import TagFieldAppearanceDemo from "@/components/docs/forms/tag-field/tag-field-appearance-demo"
-import TagFieldDemo from "@/components/docs/forms/tag-field/tag-field-demo"
-
 export default function Page() {
-  return (
-    <div>
-      <TagFieldDemo />
-      <TagFieldAppearanceDemo />
-    </div>
-  )
+  return <div />
 }
