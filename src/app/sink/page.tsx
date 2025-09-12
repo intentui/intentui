085f024a9468048c584a54e55148b12d4e2dcbed
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-09-12 16:32:59 +0700

    clean sink

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,11 +1,5 @@
 "use client"
 
-import ToastStatusDemo from "@/components/docs/statuses/toast/toast-status-demo"
-
 export default function Page() {
-  return (
-    <div className="grid grid-cols-2 gap-6 p-6">
-      <ToastStatusDemo />
-    </div>
-  )
+  return <div />
 }
