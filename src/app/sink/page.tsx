653e3dc2682495750e3905eaf858e1f5f23f8b97
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-10-04 18:48:15 +0700

    clean sink

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,16 +1,5 @@
-"use client"
-
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
-
-export default function Component() {
+export default function Page() {
   return (
-    <div className="p-4">
-      <ToggleGroup>
-        <ToggleGroupItem id="1d">1d</ToggleGroupItem>
-        <ToggleGroupItem id="3d">3d</ToggleGroupItem>
-        <ToggleGroupItem id="7d">7d</ToggleGroupItem>
-        <ToggleGroupItem id="2w">2w</ToggleGroupItem>
-      </ToggleGroup>
-    </div>
-  )
+    <div/>
+  );
 }
