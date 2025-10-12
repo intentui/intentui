044327e8b338cd90fd1eef49f201c3a75b3cdcd9
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-10-12 13:54:47 +0700

    wip

--- src/scripts/generate-search.ts
+++ src/scripts/generate-search.ts
@@ -6,7 +6,7 @@ const sectionOrder = ["prologue", "getting-started", "dark-mode", "components"]
 
 const rawStatusMap: Record<"new" | "updated" | "beta" | "alpha", string[]> = {
   new: [],
-  updated: ["modal", "sheet", "grid-list", "calendar", "range-calendar", "choice-box"],
+  updated: [],
   beta: [],
   alpha: [],
 }
