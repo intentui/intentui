6d9eef351662f06ce45a4b13495fc2261e4c2221
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-11-05 03:29:27 +0700

    wip

--- src/app/(app)/components/(partials)/list-components.tsx
+++ src/app/(app)/components/(partials)/list-components.tsx
@@ -18,25 +18,22 @@ export function ListComponents() {
     <div>
       {components?.children?.map((item) => (
         <div className="mb-12 flex break-inside-avoid flex-col gap-y-2" key={item.subsection}>
-          <h3 className="font-semibold text-fg text-sm">{item?.subsection}</h3>
+          <h3 className="mb-2 font-semibold text-base/6 text-fg">{item?.subsection}</h3>
           <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
             {item?.children?.map((item) => {
               const name = item.slug.match(/([^/]+)\/?$/)?.[1] ?? ""
               const suffix = mounted && resolvedTheme === "dark" ? "-dark" : ""
               return (
                 <li key={item.slug}>
-                  <Image
-                    className="rounded-xl ring ring-fg/10"
-                    width={708}
-                    height={480}
-                    src={`/images/thumbnails/${name}${suffix}.png`}
-                    alt={item.title}
-                  />
-                  <Link
-                    className="block py-2 text-muted-fg text-sm hover:text-fg focus:text-fg"
-                    href={item.slug}
-                  >
-                    {item.title}
+                  <Link className="flex flex-col gap-y-2 hover:opacity-80" href={item.slug}>
+                    <Image
+                      className="rounded-xl ring ring-fg/10"
+                      width={708}
+                      height={480}
+                      src={`/images/thumbnails/${name}${suffix}.png`}
+                      alt={item.title}
+                    />
+                    <span className="text-sm/6">{item.title}</span>
                   </Link>
                 </li>
               )
