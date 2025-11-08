07df457995962b254c201cad9a2098d53c9da888
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-11-08 15:37:09 +0700

    fix forms

--- src/components/ui/date-range-picker.tsx
+++ src/components/ui/date-range-picker.tsx
@@ -33,7 +33,7 @@ export function DateRangePicker<T extends DateValue>({
   return (
     <DateRangePickerPrimitive
       data-slot="control"
-      className={cx(fieldStyles(), className)}
+      className={cx(fieldStyles({ className: "group/date-range-picker" }), className)}
       {...props}
     >
       {(values) => (
@@ -53,10 +53,12 @@ export function DateRangePickerTrigger({
   return (
     <InputGroup
       className={cx(
-        "flex items-center rounded-lg",
-        "border border-input enabled:hover:border-muted-fg/30",
-        "focus-within:border-ring/70 focus-within:outline-hidden focus-within:ring-3 focus-within:ring-ring/20 focus-within:enabled:hover:border-ring/80",
-        "invalid:border-danger-subtle-fg/70 focus-within:invalid:border-danger-subtle-fg/70 focus-within:invalid:ring-danger-subtle-fg/20 invalid:hover:border-danger-subtle-fg/80 focus-within:invalid:hover:border-danger-subtle-fg/80",
+        "flex items-center overflow-hidden rounded-lg",
+        "border border-input hover:enabled:border-muted-fg/30",
+        "group-open/date-range-picker:border-ring/70 group-open/date-range-picker:bg-primary-subtle/5 group-open/date-range-picker:outline-hidden group-open/date-range-picker:ring-3 group-open/date-range-picker:ring-ring/20 group-open/date-range-picker:hover:border-ring/80",
+        "focus-within:border-ring/70 focus-within:bg-primary-subtle/5 focus-within:outline-hidden focus-within:ring-3 focus-within:ring-ring/20 focus-within:enabled:hover:border-ring/80",
+        "invalid:border-danger-subtle-fg/70 invalid:bg-danger-subtle/5 focus-within:invalid:border-danger-subtle-fg/70 focus-within:invalid:bg-danger-subtle/5 focus-within:invalid:ring-danger-subtle-fg/20 invalid:hover:border-danger-subtle-fg/80 focus-within:invalid:hover:border-danger-subtle-fg/80",
+        "disabled:bg-muted",
         className,
       )}
       {...props}
@@ -64,7 +66,7 @@ export function DateRangePickerTrigger({
       <DateInput slot="start" />
       <span
         aria-hidden="true"
-        className="-mx-3 sm:-mx-2 pointer-events-none block h-0.5 w-2 shrink-0 self-center rounded-full bg-fg group-disabled:text-opacity-50 forced-colors:text-[ButtonText] forced-colors:group-disabled:text-[GrayText]"
+        className="-mx-3 sm:-mx-2 pointer-events-none z-10 block h-0.5 w-2 shrink-0 self-center rounded-full bg-fg group-disabled/date-range-picker:text-opacity-50 forced-colors:text-[ButtonText] forced-colors:group-disabled/date-range-picker:text-[GrayText]"
       />
       <DateInput slot="end" />
       <Button
