c6c695b39077f4a4d11fd36e0af508a917be09c8
Your Name <irsyad@parsinta.com>
2025-12-28 16:45:37 +0700

    clean sink

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,11 +1,3 @@
-import SheetFloatDemo from "@/components/docs/overlays/sheet/sheet-float-demo"
-import SheetStickyDemo from "@/components/docs/overlays/sheet/sheet-sticky-demo"
-
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center">
-      <SheetStickyDemo />
-      <SheetFloatDemo />
-    </div>
-  )
+  return <div />
 }
