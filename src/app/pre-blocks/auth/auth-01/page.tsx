bdae49f0c23f7aa125abc8459de78a39a19dc1b6
Your Name <irsyad@parsinta.com>
2026-01-20 06:57:56 +0700

    add login blocks

---
+++ src/app/pre-blocks/auth/auth-01/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { Avatar } from "@/components/ui/avatar"
+import { Link } from "@/components/ui/link"
+import { Form } from "react-aria-components"
+import { Button } from "@/components/ui/button"
+import { Checkbox, CheckboxLabel } from "@/components/ui/checkbox"
+import { Description, FieldError, Fieldset, Label, Legend } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Text, TextLink } from "@/components/ui/text"
+import { TextField } from "@/components/ui/text-field"
+import { useState } from "react";
+
+export default function Page() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  return (
+    <main className="flex min-h-dvh items-center justify-center p-6">
+      <div className="w-full max-w-sm">
+        <h1 className="sr-only">Sign in</h1>
+        <Link href="#" aria-label="Goto homepage" className="mb-3 inline-block">
+          <Avatar isSquare src="https://design.intentui.com/logo" size="md" />
+        </Link>
+        <Form>
+          <Fieldset>
+            <Legend className="text-xl/6">Sign in</Legend>
+            <Text>
+              Access your account to manage projects, view analytics, and collaborate with your team.
+            </Text>
+            <TextField isRequired value={email} onChange={setEmail}>
+              <Label>Email address</Label>
+              <Input type="email" placeholder="Your email address" />
+              <FieldError />
+            </TextField>
+            <TextField isRequired value={password} onChange={setPassword}>
+              <div className="mb-2 flex items-center justify-between">
+                <Label>Password</Label>
+                <TextLink href="#" className="text-base/6 sm:text-sm/6">
+                  Forgot password?
+                </TextLink>
+              </div>
+              <Input placeholder="Ssshtt, it's a secret" type="password" />
+              <FieldError />
+            </TextField>
+            <Checkbox>
+              <CheckboxLabel>Remember me</CheckboxLabel>
+              <Description>Keep me signed in on this device for faster access next time.</Description>
+            </Checkbox>
+          </Fieldset>
+          <Button type="submit" className="mt-6 w-full">
+            Sign in
+          </Button>
+        </Form>
+      </div>
+    </main>
+  )
+}
