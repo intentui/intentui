b111ae0f186f23fa26078926760417d8fc46725b
Your Name <irsyad@parsinta.com>
2026-01-20 07:20:17 +0700

    small-fix: og-image

--- src/app/(app)/blocks/page.tsx
+++ src/app/(app)/blocks/page.tsx
@@ -8,6 +8,40 @@ export const metadata: Metadata = {
   title: "Blocks",
   description:
     "Blocks provides a comprehensive library of example guides that show how to use each component end to end, from setup to real world implementation.",
+  keywords: [
+    "Intent UI Blocks",
+    "Tailwind CSS",
+    "UI Components",
+    "UI Kit",
+    "UI Library",
+    "UI Framework",
+    "Intent UI",
+    "React Aria",
+    "React Aria Components",
+    "Server Components",
+    "React Components",
+    "Next UI Components",
+    "UI Design System",
+    "UI for Laravel Inertia",
+    "Laravel Inertia UI Library",
+    "Laravel Inertia UI Framework",
+    "Laravel Inertia Intent",
+    "Laravel Intent",
+    "Intent Components",
+    "Intent UI Components",
+    "Intent UI Kit",
+    "Intent UI Library",
+    "Intent UI Framework",
+    "Intent Laravel Inertia",
+    "Intent Laravel",
+    "Intent Inertia",
+  ],
+  authors: [
+    {
+      name: "Irsyad",
+      url: "https://x.com/irsyad",
+    },
+  ],
 }
 
 export default function Page() {
