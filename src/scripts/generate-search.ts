b7c7d843260afa6f5d7490af7fec1d67c34f9ab4
Your Name <irsyad@parsinta.com>
2026-01-27 17:14:42 +0700

    refactor radius

--- src/scripts/generate-search.ts
+++ src/scripts/generate-search.ts
@@ -6,7 +6,7 @@ const sectionOrder = ["prologue", "getting-started", "dark-mode", "components"]
 
 const rawStatusMap: Record<"new" | "updated" | "beta" | "alpha", string[]> = {
   new: [],
-  updated: ["area-chart", "avatar", "badge", "bar-chart", "bar-list", "breadcrumbs", "button-group", "button", "card", "chart", "choice-box", "color-area", "color-picker", "color-slider", "color-swatch-picker", "color-swatch", "color-thumb", "color-wheel", "combo-box", "command-menu", "container", "context-menu", "date-field", "date-picker", "date-range-picker", "description-list", "dialog", "disclosure-group", "drop-zone", "dropdown", "field", "file-trigger", "grid-list", "heading", "input", "keyboard", "leaderboard", "line-chart", "link", "list-box", "loader", "menu", "meter", "modal", "multiple-select", "navbar", "note", "number-field", "pagination", "pie-chart", "popover", "progress-bar", "radio", "range-calendar", "select", "separator", "sheet", "show-more", "sidebar", "skeleton", "slider", "snippet", "switch", "table", "tabs", "tag-field", "tag-group", "text-field", "text", "time-field", "toast", "toggle", "toolbar", "tooltip"],
+  updated: ["button-group", "input-otp", "toggle-group", "tracker"],
   beta: [],
   alpha: [],
 }
