ffd12423461b47cf48324e0f83eeb0590ab7ab7c
Your Name <irsyad@parsinta.com>
2026-02-05 11:17:48 +0700

    add render to docs button

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,20 +1,5 @@
-'use client'
-
-import { Button } from '@/components/ui/button';
-import { motion } from 'motion/react';
-
+"use client"
 
 export default function Page() {
-  return (
-    <div>
-      <Button
-        render={(domProps, { isPressed }) => (
-          <motion.button
-            {...domProps}
-            animate={{ scale: isPressed ? 0.9 : 1 }}/>
-        )}>
-        Press me
-      </Button>
-    </div>
-  )
+  return <div />
 }
