e2029c777aea64e28e08733721b44a2b57b0c67b
Your Name <irsyad@parsinta.com>
2026-03-10 03:46:28 +0700

    add default bg

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -17,9 +17,8 @@ export function Input({ className, ref, ...props }: InputProps) {
     <span data-slot="control" className="relative block w-full">
       <InputPrimitive
         ref={ref}
-        data-slot="input"
         className={cx(
-          "relative block w-full appearance-none rounded-lg px-[calc(--spacing(3.5)-1px)] py-[calc(--spacing(2.5)-1px)] sm:px-[calc(--spacing(3)-1px)] sm:py-[calc(--spacing(1.5)-1px)]",
+          "bg-(--control-bg,transparent) relative block w-full appearance-none rounded-lg px-[calc(--spacing(3.5)-1px)] py-[calc(--spacing(2.5)-1px)] sm:px-[calc(--spacing(3)-1px)] sm:py-[calc(--spacing(1.5)-1px)]",
           "text-base/6 text-fg placeholder:text-muted-fg sm:text-sm/6",
           "border border-input enabled:hover:border-muted-fg/30",
           "outline-hidden focus:border-ring/70 focus:ring-3 focus:ring-ring/20 focus:enabled:hover:border-ring/80",
