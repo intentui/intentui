5f9f05c7c4abd1632e94ca82dfe3e2dc26e76a78
Your Name <irsyad@parsinta.com>
2026-03-10 03:56:03 +0700

    release notes

--- src/app/sink/page.tsx
+++ src/app/sink/page.tsx
@@ -1,10 +1,4 @@
 "use client"
-
-import ComboBoxCustomValueDemo from "@/components/docs/pickers/combo-box/combo-box-custom-value-demo";
-import TextareaDemo from "@/components/docs/forms/textarea/textarea-demo";
-import SelectDemo from "@/components/docs/pickers/select/select-demo";
-import TextFieldDemo from "@/components/docs/forms/text-field/text-field-demo";
-
 export default function Page() {
   return <div/>
 }
